97391a07e953ecf1d3a777800561d8105f2f0a72
monirhosenakanda <monirhosenakanda@gmail.com>
2022-02-19 00:45:21 +0600

    first commit

---
+++ basic.js
@@ -0,0 +1,48 @@
+//variable
+var deposit = 400;
+//condition
+if(deposit > 500){
+
+}
+if(deposit < 20){
+
+}
+if (deposit == 500)
+{
+
+}
+if (deposit != 500){
+
+}
+if(deposit >= 500){
+
+}
+
+//array
+
+const number = [45, 567, 124,34,5, 56,];
+const numberCount = number.length;
+number.push(111);
+number.pop()
+number [2] = 555;
+if(number.indexOf(222) !=-1){
+
+}
+if(number.includes(222)){
+
+}
+//loop
+//while
+for (const numbers of number){
+
+}
+
+//function
+
+function fullName (first , second){
+    const name = first + '' + second;
+}
+const person = fullName('Rahim','Ali');
+
+//object 
+const bottle = {color:'yellow' , contain:'water', price:'50', dot:'yes'}
