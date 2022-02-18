a9d3e08a345bd1d90e331c26720607a56729ac4b
monirhosenakanda <monirhosenakanda@gmail.com>
2022-02-19 02:07:14 +0600

    let const and var

---
+++ let-const.js
@@ -0,0 +1,16 @@
+let balance = 1240;
+balance = 1340;
+const userName = 'Jan Paki potash potash';
+const weTogethor = 'Ami' +' '+ userName;
+console.log(weTogethor);
+const numbers = [34, 45 , 4555, 345, 4545, 34455];
+// numbers = [77, 44, 4555, 4545, 45454,44];
+const student = {roll: 100, name: 'mofiz' , job: 'intern'};
+student.name = 'kamal';
+let sum = 0;
+for(let i=0; i<numbers.length; i++){
+    const number = numbers[i];
+    console.log(number);
+    sum = sum + number;
+
+}
