29d5212be1d93260f8e0be7fd489ba0c8cbf2d7d
monirhosenakanda <monirhosenakanda@gmail.com>
2022-02-19 09:48:24 +0600

    dynamic string

---
+++ template-string.js
@@ -0,0 +1,28 @@
+const priys = 'Asif Akbar The Great';
+const meye = "Meye Tumi ki Dukkho Chino";
+const kobita = `Kobita tumi ki sopno charini`;
+
+const multiline = 'This is First Line \n'+ 
+'this is my second line \n' + 
+'this is third line \n' + 
+'this fourth line end of line';
+
+const multilineNew = `this is multiline
+this is second line 
+this is third line 
+tihis is fourth line
+this is another line
+this is end of line`;
+console.log(multiline)
+
+const friendCount = 5;
+const oldHeader = '<h3 class="friend-name">Friend</h3>'
+const oldHeader2 = '<h3 class = "friend-name">Friend'+ friendCount + '</h3>'
+
+const new1 = `<h3 class="friend-name">Friend-${friendCount}</h3>`;
+console.log(new1);
+
+const frist = 'Mamun';
+const last = 'Chowdhary';
+const full = `This person name is:${frist} ${last}.`;
+console.log(full);
